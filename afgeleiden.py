74615b643f36b446e8636f8fe553ca5e9a01660b
Nathan Tromp <103584549+NathanCabeiDiWebo@users.noreply.github.com>
2022-12-03 11:29:23 +0100

    Add files via upload

---
+++ afgeleiden.py
@@ -0,0 +1,52 @@
+"""
+Dit programma berekent de vergelijking voor de raaklijn van de grafiek met de vorm: y=ax^n + bx
+Gemaakt door Nathan Tromp
+"""
+# imports
+import math
+
+
+# Module voor formule
+def display():
+    txt = "De vergelijking is: f(x) = {} x^{} + {}x"
+    print(txt.format(a, n, b, ))
+    print("\n")
+
+
+# Module voor de afgeleide
+def afgeleide():
+    A = (a * n)
+    N = (n - 1)
+    txt1 = "De vergelijking is: f'(x) = {} x^{} + {} "
+    print(txt1.format(A, N, b))
+    print("\n")
+
+
+# Module voor de raaklijn
+def raaklijn():
+    c =
+    print("Formule van de raaklijn is y = cx + d ")
+    print("\n")
+
+
+# Inputs
+a = int(input("Voer een getal voor a in:"))
+print("Getal voor a: ", a)
+n = int(input("Voer een getal voor n in:"))
+print("Getal voor n: ", n)
+b = int(input("Voer een getal voor b in:"))
+print("Getal voor b: ", b)
+x = int(input("Voer een getal in voor x:"))
+print("Getal voor x: ", x)
+
+
+# main code
+print("Hallo, bedankt voor het openen van dit programma :)")
+print("\n")
+print("Dit programma berekent de vergelijking voor ")
+print("de raaklijn van de grafiek met de vorm: y = ax^n + bx.")
+
+
+# initialiseer modules
+display()
+afgeleide()

60713baee345bd078a048922b3980e2d8de07d17
Nathan Tromp <103584549+NathanCabeiDiWebo@users.noreply.github.com>
2022-12-05 02:42:14 +0100

    Update afgeleiden.py

--- afgeleiden.py
+++ afgeleiden.py
@@ -1,52 +1,80 @@
 """
-Dit programma berekent de vergelijking voor de raaklijn van de grafiek met de vorm: y=ax^n + bx
-Gemaakt door Nathan Tromp
+lmao
 """
 # imports
 import math
 
 
-# Module voor formule
+# what nibbas see on the screen
+def screen():
+    print("\n")
+    print("Dit programma berekent de vergelijking van ")
+    print("de raaklijn van de grafiek met de vorm: y = ax^n + bx.")
+
+
+# Module for formule
 def display():
     txt = "De vergelijking is: f(x) = {} x^{} + {}x"
     print(txt.format(a, n, b, ))
     print("\n")
 
 
-# Module voor de afgeleide
+# Module derivative
 def afgeleide():
+    global A, N, c
     A = (a * n)
     N = (n - 1)
-    txt1 = "De vergelijking is: f'(x) = {} x^{} + {} "
-    print(txt1.format(A, N, b))
-    print("\n")
+    c = pow (x,N)
+    c *= A
+    c += b
 
 
-# Module voor de raaklijn
+# Module raaklijn
 def raaklijn():
-    c =
-    print("Formule van de raaklijn is y = cx + d ")
+    d = y - c1
+    if d < 0:
+        d *= -1
+        txt4 = "De vergelijking van de raaklijn is y = {}x - {} "
+        print(txt4.format(c, d))
+        print("\n")
+    else:
+        txt5 = "De vergelijking van de raaklijn is y = {}x + {} "
+        print(txt5.format(c, d))
+        print("\n")
+
+def msg():
+    global y, c1
+    y = pow(x, n)
+    y *= a
+    y += (b * x)
+    c1 = c
+    c1 *= x
+    txt3 = "Het raakpunt is ({} is {})"
+    print(txt3.format(x,y))
+    print("\n")
+
+def helling():
+    txt3 = "De helling op x = {} is {}"
+    print(txt3.format(x,c))
     print("\n")
 
 
-# start screen
-print("Hallo, bedankt voor het openen van dit programma :)")
-print("\n")
-print("Dit programma berekent de vergelijking voor ")
-print("de raaklijn van de grafiek met de vorm: y = ax^n + bx.")
-
-
-# Inputs
-a = int(input("Voer een getal voor a in:"))
+#main code
+screen()
+a = int(input("Voer in a: "))
 print("Getal voor a: ", a)
-n = int(input("Voer een getal voor n in:"))
+n = int(input("Voer in n: "))
 print("Getal voor n: ", n)
-b = int(input("Voer een getal voor b in:"))
+b = int(input("Voer in b: "))
 print("Getal voor b: ", b)
-x = int(input("Voer een getal in voor x:"))
+x = int(input("Voer in x: "))
 print("Getal voor x: ", x)
+print("\n")
 
 
-# initialiseer modules
+#rest of modules
 display()
 afgeleide()
+msg()
+helling()
+raaklijn()
